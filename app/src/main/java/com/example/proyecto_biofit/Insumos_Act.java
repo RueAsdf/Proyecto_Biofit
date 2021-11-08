f2577155e65a409caf7c4743ae6255f09c84d626
RueAsdf <Ruej20@gmail.com>
2021-11-08 04:46:19 -0300

    -Asignado el metodo para el boton calcular //1

--- app/src/main/java/com/example/proyecto_biofit/Insumos_Act.java
+++ app/src/main/java/com/example/proyecto_biofit/Insumos_Act.java
@@ -3,12 +3,15 @@ package com.example.proyecto_biofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Insumos_Act extends AppCompatActivity {
 
     private Spinner spinsumos;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,23 @@ public class Insumos_Act extends AppCompatActivity {
         setContentView(R.layout.activity_insumos);
 
         spinsumos = findViewById(R.id.spnInsumos);
+        result = findViewById(R.id.result);
 
         Bundle bun = getIntent().getExtras();
         String[] listadeinsumos = bun.getStringArray("insumos");
 
         ArrayAdapter adaptInsumos = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listadeinsumos);
         spinsumos.setAdapter(adaptInsumos);
+    }
+
+    public void Calcular(View view){
+
+        String opcion = spinsumos.getSelectedItem().toString();
+
+        for(int i = 0; i < opcion.length(); i++){
+            if (opcion.equals(opcion)){
+                result.setText("La opcion seleccionada es: " + opcion);
+            }
+        }
     }
 }
