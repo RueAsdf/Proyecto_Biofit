dde96a859a5efa36f19c20e89d454a30226ca7be
BRAVOSIX <claudio_jr@live.cl>
2021-11-08 15:47:06 -0300

    Se crea la clase 'Database' para importar la bd

---
+++ app/src/main/java/database/Database.java
@@ -0,0 +1,24 @@
+package database;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+    //Constructor para instanciar la base de datos
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //Método para poder crear mi modelo de trabajo
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+    //Método para realizar actualizaciones o cambios en mi modelo
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
