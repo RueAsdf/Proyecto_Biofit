5eaec383b7c0ff4eb7d7a93550416eaaad540a71
RueAsdf <Ruej20@gmail.com>
2021-11-08 02:49:15 -0300

    - Inicio de sesion por medio de objeto Administrador -Validacion de errores en loggin

---
+++ app/src/main/java/Objetos/Administrador.java
@@ -0,0 +1,43 @@
+package Objetos;
+
+public class Administrador {
+    private int id;
+    private String user;
+    private String pass;
+
+    public Administrador (){
+        user = "diego";   // Inicializar valores por defecto
+        pass = "123";
+    }
+
+    public Administrador(int id, String user, String pass) {
+        this.id = id;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+}
--- app/src/main/java/com/example/proyecto_biofit/MainActivity.java
+++ app/src/main/java/com/example/proyecto_biofit/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.proyecto_biofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import Objetos.Administrador;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText user, pass;
+    private TextView msj;
+    private Administrador adm = new Administrador();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = findViewById(R.id.txtUser);
+        pass = findViewById(R.id.txtPass);
+        msj  = findViewById(R.id.msj);
+
+    }
+
+    public void LoadSession (View view){
+        String userObj    = adm.getUser().toString().trim();
+        String passObj    = adm.getPass().toString().trim();
+
+        String usuario    = user.getText().toString().trim();
+        String contrasena = pass.getText().toString().trim();
+
+        switch (usuario){
+            case "diego" :
+                if (usuario.equals(userObj) && contrasena.equals(passObj)){
+                    Intent i = new Intent(this, Insumos_Act.class);
+                    startActivity(i);
+                }
+                break;
+            case "":
+                if (usuario.equals("") && contrasena.equals("")){
+                    msj.setText("Campos vacios, por favor llenar campos");
+                }
+                break;
+            default:
+                if (!usuario.equals(userObj) && !contrasena.equals(passObj)){
+                    msj.setText("Campos incorrectos");
+                }
+                break;
+        }
     }
 }
