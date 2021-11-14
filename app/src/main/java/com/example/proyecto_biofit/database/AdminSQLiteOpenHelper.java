c3e4b3bf9fa2b53281564594b77afed778445e93
BRAVOSIX <claudio_jr@live.cl>
2021-11-14 19:56:50 -0300

    Se crea directorio de db y clase 'AdminSQLiteOpenHelper'

---
+++ app/src/main/java/com/example/proyecto_biofit/database/AdminSQLiteOpenHelper.java
@@ -0,0 +1,15 @@
+package com.example.proyecto_biofit.database;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    //Método para poder crear modelo de trabajo
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+    //Método para realizar actualizaciones en el modelo
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
